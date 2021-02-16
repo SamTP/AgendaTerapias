cc04094cdf55d1e44fe585e2912208aaf2d90f18
SamTP <39800905+SamTP@users.noreply.github.com>
2021-02-16 12:10:08 -0600

    Login View

--- src/app/nuevaTerapia/nuevaTerapia.component.ts
+++ src/app/nuevaTerapia/nuevaTerapia.component.ts
@@ -1,7 +1,18 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nueva-terapia',
   templateUrl: './nuevaTerapia.component.html',
 })
-export class NuevaTerapiaComponent {}
+export class NuevaTerapiaComponent {
+  private paciente: String = null;
+  private fecha: Date = null;
+  private hora: Time = null;
+
+  private cita = {
+    paciente: this.paciente,
+    fecha: this.fecha,
+    hora: this.hora,
+  };
+}
